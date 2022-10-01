5043cff982042a7daeccd27f398e609abf95d36d
usernamehacktoberfest <114504277+usernamehacktoberfest@users.noreply.github.com>
2022-10-01 18:04:32 +0530

    Create hello_world_hacktober.cpp

---
+++ Contribution/hello_world_hacktober.cpp
@@ -0,0 +1,18 @@
+// LANGUAGE: C++
+// ENV: dev C++
+// AUTHOR: Kp
+// GITHUB: https://github.com/usernamehacktoberfest
+// LINK: http://es6-features.org/
+
+// This hello world uses C++
+// Driver function has cout which prints Hello World
+
+
+//C++
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  cout << "Hello World";
+  return 0;
+}
